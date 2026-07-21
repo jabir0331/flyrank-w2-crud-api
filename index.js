25bcb96ecb6446f3069cd955370d087259e9dc49
jabir0331 <mnnjabir@gmail.com>
2026-07-22 00:25:59 +0530

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Hello World!",
+    "name": "Task API", 
+    "version": "1.0", 
+    "endpoints": ["/tasks"] 
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
   });
 });
 
