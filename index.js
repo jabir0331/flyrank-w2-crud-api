2e37361e4c0df8839476f60ca7d5c011671e8768
jabir0331 <mnnjabir@gmail.com>
2026-07-22 18:06:44 +0530

    Commit: Stage 5: Swagger UI

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./openapi.json");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const PORT = 3000;
 
